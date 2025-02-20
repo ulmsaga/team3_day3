c42a71c90f81469275434c58fc51bb8ff4dc77de
ulmsaga <sclee1115@gmail.com>
2025-02-20 17:21:45 +0900

    day3 init

---
+++ accounts/src/test/java/com/eazybytes/accounts/AccountsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.eazybytes.accounts;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ eurekaserver/src/test/java/com/eazybytes/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,15 @@
+package com.eazybytes.eurekaserver;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
